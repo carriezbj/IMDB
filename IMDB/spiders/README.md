942471e67ebb610e0dd4e02cf55ae29d61ac3f85
carriezbj <bingjie.zhang1991@gmail.com>
2018-04-24 21:19:26 -0700

    Parameterize movie review

---
+++ IMDB/spiders/README.md
@@ -0,0 +1,12 @@
+# Notes:
+
+movie_review.py is a spider code that crawls all reviews under a certain movie or user
+
+## Usage:
+Use below command to kick off 
+```sh
+scrapy crawl movie_reviews -a id_type=id_type -a id=id
+```
+
+- id_type can be either title or user
+- id is id of corresponding item 
--- IMDB/spiders/movie_reviews.py
+++ IMDB/spiders/movie_reviews.py
@@ -4,24 +4,30 @@ import urllib
 class QuotesSpider(scrapy.Spider):
     name = "movie_reviews"
     allowed_domains = ['www.imdb.com']
-    start_urls = [
-        'https://www.imdb.com/title/tt6791096/reviews?ref_=tt_urv'
-    ]
+    base_url = "https://www.imdb.com"
+
+    def start_requests(self):
+        # Note: id_type can only be two values, title/user
+        # ID is either user_id or title_id
+        yield scrapy.Request(self.base_url + '/{}/{}/reviews?ref_=tt_urv'.format(self.id_type,self.id))
 
     def parse(self, response):
-        base_url = "https://www.imdb.com"
+        
         for review in response.css('div.review-container'):
+            user_id = review.css('.display-name-link a::attr(href)').extract_first().split('/')[2] if self.id_type == 'title' else self.id
+            title_id = review.css('.lister-item-header a::attr(href)').extract_first().split('/')[2] if self.id_type == 'user' else self.id
+            #user_name = review.css('.display-name-link a::text').extract_first()
             yield {
                 'rating': review.css('span.rating-other-user-rating span::text').extract_first(),
                 'date': review.css('.review-date::text').extract_first(),
-                'user_name': review.css('.display-name-link a::text').extract_first(),
-                'user_id': review.css('.display-name-link a::attr(href)').extract_first().split('/')[2],
+                'title_id': title_id,
+                'user_id': user_id,
                 'title': review.css('.title::text').extract_first(),
                 'content': '\n'.join(review.css('.content div.text::text').extract())
             }
         data_key = response.css('.load-more-data::attr(data-key)').extract_first()
-        ajax_url = '/title/tt6791096/reviews/_ajax?'
+        ajax_url = '/{}/{}/reviews/_ajax?'.format(self.id_type,self.id)
         self.logger.info('Data Key: %s', data_key)
         parameters = {"ref_": "undefined","paginationKey":data_key}
         if data_key is not None and ajax_url is not None:
-            yield scrapy.Request(base_url + ajax_url + urllib.parse.urlencode(parameters),callback = self.parse)
+            yield scrapy.Request(self.base_url + ajax_url + urllib.parse.urlencode(parameters),callback = self.parse)
